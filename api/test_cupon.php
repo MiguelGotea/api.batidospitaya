d75114ff1251f809b4a97dfa0edf4ba47c89fdfe
BatidosPitaya <miguelgotea.1@gmail.com>
2026-02-18 18:19:56 -0600

    2026-02-18 18:19:56

--- api/test_cupon.php
+++ api/test_cupon.php
@@ -10,7 +10,7 @@ require_once __DIR__ . '/../core/database/conexion.php';
 
 // === CONFIGURACIÓN ===
 $api_token = 'a8f5e2d9c4b7a1e6f3d8c5b2a9e6d3f0c7a4b1e8d5c2a9f6e3d0c7b4a1e8f5d2';
-$url = 'http://api.batidospitaya.com/api/aplicar_cupon.php';
+$url = 'https://api.batidospitaya.com/api/aplicar_cupon.php'; // Cambiado a HTTPS para evitar redirecciones que pierden headers
 
 // Datos de prueba
 $numero_cupon = '467465170';
